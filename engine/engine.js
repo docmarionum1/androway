b671ea5b8bf0f13114fddfdf0f6db82d2c54d21d
Excurion <admin@excurion.nl>
2009-11-27 16:41:22 +0100

    Added fileheaders and comments

--- engine/engine.js
+++ engine/engine.js
@@ -1,2 +1,17 @@
+/**
+ * Engine::Core
+ * 
+ * LICENSE
+ * 
+ * This source file is subject to the GPL license that is bundled
+ * with this package in the file LICENSE
+ * 
+ * @package     androway
+ * @copyright   Copyright (c) 2009, DaveC
+ * @license     http://www.gnu.org/licenses/gpl.txt GPL
+ */
+ 
 includeJS("engine/graphics/core.graphics.texture.js");
 includeJS("engine/graphics/core.graphics.texturemanager.js");
+
+
--- library/global.js
+++ library/global.js
@@ -1,3 +1,22 @@
+/**
+ * Library::Global
+ * 
+ * LICENSE
+ * 
+ * This source file is subject to the GPL license that is bundled
+ * with this package in the file LICENSE
+ * 
+ * @package     androway
+ * @copyright   Copyright (c) 2009, DaveC
+ * @license     http://www.gnu.org/licenses/gpl.txt GPL
+ */
+ 
+/**
+ * Dynamic Javascript file include function
+ * 
+ * @param string file - the relative to viewed page location of the javascript file
+ * @return void
+ */
 function includeJS(file){
     if($("script[src='"+file+"']").size() == 0){
         $("head").createAppend(
