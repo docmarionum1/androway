a299bb3fd98a74ff762c296c1b1c79bd72f3daed
Pascal Scheepers <pascal@EXC-MAIN.(none)>
2010-01-09 18:57:59 +0100

    Added threading initialisation sections to the engine

--- engine/engine.js
+++ engine/engine.js
@@ -4,17 +4,28 @@
  * The game-engine's core
  * 
  * @function	public		initialize		-	Initializes the engine
- * @
+ * @function	public		PickMultiThreadMethod	-	Picks wether Google Gears needs to be used, or Workers
+ * @function	public		CreateThreads		-	Creates the threads, and sets up communications
  */
 var Engine = new Class({
   settings: {
     useGears: false,
     useWorkers: false
   },
+  threads: {
+    comm: {
+      thread: null,
+      id: 0
+    },
+    AI: {
+      thread: null,
+      id: 0
+    }
+  },
   initialize: function(){
     if(!this.PickMultiThreadMethod()) return false;
     
-    
+    this.CreateThreads();
   },
   PickMultiThreadMethod: function(){
     if(Worker){
@@ -36,6 +47,46 @@ var Engine = new Class({
         
         return true;
       }
+    }
+  },
+  CreateThreads: function(){
+    if(this.settings.useWorkers){
+      //Create communications worker
+      this.threads.comm.thread = new Worker("engine/engine.comm.js");
+      
+      this.threads.comm.thread.onmessage = function(message){
+        
+      }
+      
+      //Create Artificial Intelligence thread
+      this.threads.AI.thread = new Worker("engine/engine.AI.js");
+      
+      this.threads.AI.thread.onmessage = function(message){
+        
+      }
+      
+      return true;
+    }
+    if(this.settings.useGears){
+      //Create communications worker
+      this.threads.comm.thread = google.gears.factory.create('beta.workerpool');
+      
+      this.threads.comm.thread.onmessage = function(message){
+        
+      }
+      
+      this.threads.comm.id = this.threads.comm.thread.createWorkerFromUrl("engine/engine.comm.js");
+      
+      //Create Artificial Intelligence thread
+      this.threads.AI.thread = google.gears.factory.create('beta.workerpool');
+      
+      this.threads.AI.thread.onmessage = function(message){
+        
+      }
+      
+      this.threads.AI.id = this.threads.AI.thread.createWorkerFromUrl("engine/engine.AI.js"); 
+      
+      return true;
     }
   }
 }
