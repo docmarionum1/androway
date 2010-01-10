5cb521ea7ab1f5e0eb6dc6fecf6b976ade43083f
docmarionum1 <docmarionum1@gmail.com>
2010-01-10 03:45:18 -0500

    Added graphics, sound and input threads.  Created function to initialize the canvas.

--- engine/engine.js
+++ engine/engine.js
@@ -6,6 +6,7 @@
  * @function	public		initialize		-	Initializes the engine
  * @function	public		PickMultiThreadMethod	-	Picks wether Google Gears needs to be used, or Workers
  * @function	public		CreateThreads		-	Creates the threads, and sets up communications
+ * @function	public		InitCanvas		-	Grabs the canvas element from the page and creates a 2d context.
  */
 var Engine = new Class({
   settings: {
@@ -20,12 +21,30 @@ var Engine = new Class({
     AI: {
       thread: null,
       id: 0
-    }
+    },
+	graphics: {
+	  thread: null,
+	  id: 0
+	},
+	sound: {
+	  thread: null,
+	  id: 0
+	},
+	input: {
+	  thread: null,
+	  id: 0
+	}
+  },
+  canvas: {
+	canvasElement: null,
+	context: null
   },
   initialize: function(){
     if(!this.PickMultiThreadMethod()) return false;
     
     this.CreateThreads();
+	
+	if (!this.initCanvas()) return false;
   },
   PickMultiThreadMethod: function(){
     if(Worker){
@@ -64,6 +83,27 @@ var Engine = new Class({
       this.threads.AI.thread.onmessage = function(message){
         
       }
+	  
+	  //Create Graphics thread
+	  this.threads.graphics.thread = new Worker("engine/engine.graphics.js");
+	  
+	  this.threads.graphics.thread.onmessage = function(message){
+	  
+	  }
+	  
+	  //Create Sound thread
+	  this.threads.sound.thread = new Worker("engine/engine.sound.js");
+	  
+	  this.threads.sound.thread.onmessage = function(message){
+	  
+	  }
+	  
+	  //Create Input thread
+	  this.threads.input.thread = new Worker("engine/engine.input.js");
+	  
+	  this.threads.input.thread.onmessage = function(message){
+	  
+	  }
       
       return true;
     }
@@ -85,8 +125,43 @@ var Engine = new Class({
       }
       
       this.threads.AI.id = this.threads.AI.thread.createWorkerFromUrl("engine/engine.AI.js"); 
+	  
+	  //Create Graphics thread
+      this.threads.graphics.thread = google.gears.factory.create('beta.workerpool');
+      
+      this.threads.graphics.thread.onmessage = function(message){
+        
+      }
+      
+      this.threads.graphics.id = this.threads.graphics.thread.createWorkerFromUrl("engine/engine.graphics.js");
+	  
+	  //Create Sound thread
+      this.threads.sound.thread = google.gears.factory.create('beta.workerpool');
+      
+      this.threads.sound.thread.onmessage = function(message){
+        
+      }
+      
+      this.threads.sound.id = this.threads.sound.thread.createWorkerFromUrl("engine/engine.sound.js");
+	  
+	  //Create Artificial Intelligence thread
+      this.threads.input.thread = google.gears.factory.create('beta.workerpool');
+      
+      this.threads.input.thread.onmessage = function(message){
+        
+      }
+      
+      this.threads.input.id = this.threads.input.thread.createWorkerFromUrl("engine/engine.input.js");
       
       return true;
     }
+  },
+  InitCanvas: function(){
+	this.canvas.canvasElement = $('canvas');
+	if (this.canvas.canvasElement && this.canvas.canvasElement.getContext){
+		this.canvas.context = this.canvas.canvasElement.getContext('2d');
+		if (this.canvas.context) return true;
+	}
+	return false;
   }
-}
+});
